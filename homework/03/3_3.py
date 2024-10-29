9cd59037864779f264095965657ac5780cffe3a8
Bohdan Danylko <malyyboh@gmail.com>
2024-10-29 21:55:13 +0100

    homework lesson 3

--- homework/03/3_3.py
+++ homework/03/3_3.py
@@ -1,22 +1,14 @@
 user_list = [1, 2, 3, 4, 5, 6]
 
-first_list = []
-second_list = []
-divided_user_list = []
-new_user_list = []
+divided_paired_user_list = len(user_list) // 2
+divided_odd_user_list = round(len(user_list) / 2 + 0.1)
 
-if len(user_list) == 0:
-    new_user_list = [first_list] + [second_list]
-    print(new_user_list)
-elif len(user_list) % 2 == 0:
-    divided_user_list = len(user_list) // 2
-    first_list = user_list[:divided_user_list]
-    second_list = user_list[divided_user_list:]
-    new_user_list = [first_list] + [second_list]
-    print(new_user_list)
+if len(user_list) % 2 == 0:
+    first_list = user_list[:divided_paired_user_list]
+    second_list = user_list[divided_paired_user_list:]
 else:
-    divided_user_list = round(len(user_list) / 2 + 0.1)
-    first_list = user_list[:divided_user_list]
-    second_list = user_list[divided_user_list:]
-    new_user_list = [first_list] + [second_list]
-    print(new_user_list)
+    first_list = user_list[:divided_odd_user_list]
+    second_list = user_list[divided_odd_user_list:]
+
+new_user_list = [first_list] + [second_list]
+print(new_user_list)
