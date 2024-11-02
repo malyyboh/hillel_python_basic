f777bcc70a6239e0648897dc83bb8e5330c06d1b
Bohdan Danylko <malyyboh@gmail.com>
2024-11-02 12:34:33 +0100

    homework lesson 4 (changed variable name)

--- homework/04/4_3.py
+++ homework/04/4_3.py
@@ -1,7 +1,7 @@
 import random
 
 lst = []
-lst_2 = []
+new_lst = []
 
 for i in range(random.randint(3, 10)):
     lst.append(random.randint(1, 10))
@@ -9,5 +9,5 @@ print(lst)
 
 for i, el in enumerate(lst):
     if i == 0 or i == 2 or i == len(lst) - 2:
-        lst_2.append(el)
-print(lst_2)
+        new_lst.append(el)
+print(new_lst)
