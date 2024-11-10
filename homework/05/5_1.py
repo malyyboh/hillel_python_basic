a014fe3ca7872a49bf8135e3924a2d0564466cca
Bohdan Danylko <malyyboh@gmail.com>
2024-11-10 11:02:20 +0100

    homework lesson 5 (5_1 added a condition when the variable name is '_')

--- homework/05/5_1.py
+++ homework/05/5_1.py
@@ -6,6 +6,10 @@ if user_string.isidentifier():
     if user_string not in keyword.kwlist and user_string.islower() and user_string.count('_') <= 1:
         print(True)
     else:
-        print(False)
+        if user_string == '_':
+            print(True)
+        else:
+            print(False)
 else:
     print(False)
+
