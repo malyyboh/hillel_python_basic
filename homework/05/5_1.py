efec88cf73d87c7844f31bfc87110b4e5c1a96e2
Bohdan Danylko <malyyboh@gmail.com>
2024-11-11 11:24:01 +0100

    homework lesson 5 (5_1 changed the condition for checking the number of '_' and added the elif condition)

--- homework/05/5_1.py
+++ homework/05/5_1.py
@@ -3,13 +3,12 @@ import keyword
 user_string = input('Please input a variable name: ')
 
 if user_string.isidentifier():
-    if user_string not in keyword.kwlist and user_string.islower() and user_string.count('_') <= 1:
+    if user_string not in keyword.kwlist and user_string.islower():
+        print(True)
+    elif user_string == '_':
         print(True)
     else:
-        if user_string == '_':
-            print(True)
-        else:
-            print(False)
+        print(False)
 else:
     print(False)
 
