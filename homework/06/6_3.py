cf8387108f4b675d700fd4bbc83a38dce401f1b2
Bohdan Danylko <malyyboh@gmail.com>
2024-11-13 22:38:18 +0100

    homework lesson 6 (6_3 delete comments)

--- homework/06/6_3.py
+++ homework/06/6_3.py
@@ -12,29 +12,3 @@ while True:
 print(user_int_input)
 
 
-
-
-
-# last_digit = user_int_input % 10
-# second_digit = user_int_input // 10 % 10
-# first_digit = user_int_input // 100 % 10
-# print(len(str(user_int_input)))
-# i = 0
-# while len(str(user_int_input)) >= i:
-#     user_int_input = int(last_digit * second_digit * first_digit)
-#     i += 1
-#     print(user_int_input)
-
-
-# int_list = list()
-# result_int = 1
-#
-# for item in user_int_input:
-#     int_list.append(int(item))
-#     result_int *= int(item)
-#
-# while len(int_list) >= 1:
-#     result_int *= item
-# print(int_list)
-# print(result_int)
-
