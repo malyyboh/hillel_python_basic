9e0e4f8c8211a4ba3fcfe02b7f73690d30ac52a4
Bohdan Danylko <malyyboh@gmail.com>
2024-12-01 23:05:35 +0100

    homework lesson 11 (11.1 - fix the correct check for a prime number)

--- homework/11/11_1.py
+++ homework/11/11_1.py
@@ -1,29 +1,25 @@
 def prime_generator(end):
     start = 2
+    non_prime_number = 0
     while start <= end:
         for i in range(2, end + 1):
             if start % i == 0 and start != i:
-                start += 1
+                non_prime_number = start
                 break
-            else:
-                yield start
-                start += 1
-                break
-
-
+        if non_prime_number != start:
+            yield start
+        start += 1
 
 
 from inspect import isgenerator
 
-gen = prime_generator(29)
-# assert isgenerator(gen) == True, 'Test0'
-# assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
-# assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
-# assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
-# print('Ok')
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
 
-for i in gen:
-    print(i)
 
 
 
