53c7eb96c00d6d68202abdac2b9e55776f4e6ebe
Bohdan Danylko <malyyboh@gmail.com>
2024-12-07 13:44:33 +0100

    homework lesson 12 (12_1 add var cleaned_html, list cleaned_html_list)

--- homework/12/12_1.py
+++ homework/12/12_1.py
@@ -5,14 +5,19 @@ def delete_html_tags(html_file, result_file='cleaned.txt'):
 
     with codecs.open(html_file, 'r', 'utf-8') as file:
         html = file.readlines()
-        for line in html:
-            if line.count('<') > 1 or line.count('>') > 1:
-                lst = line.split('>')
-                lst = lst[1].split('<')
-                if len(lst[0]) >= 1:
-                    line = lst[0] + '\n'
-                    with codecs.open(result_file, 'a', 'utf-8') as file:
-                        file.write(line)
+
+    cleaned_html_lst = []
+    for line in html:
+        if line.count('<') > 1 or line.count('>') > 1:
+            lst = line.split('>')
+            lst = lst[1].split('<')
+            if len(lst[0]) >= 1:
+                line = lst[0]
+                cleaned_html_lst.append(line)
+    cleaned_html = '\n'.join(cleaned_html_lst)
+
+    with codecs.open(result_file, 'w', 'utf-8') as file:
+        file.write(cleaned_html)
 
 
 delete_html_tags('draft.html', result_file='cleaned.txt')
