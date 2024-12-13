649246d629883db0b1c83182f290f66b6689b0f2
Bohdan Danylko <malyyboh@gmail.com>
2024-12-13 18:49:32 +0100

    homework lesson 14 (13_1 added class MaxNumberOfStudent(Exception) and added a condition to the method add_student in the class Group)

--- homework/13/13_1.py
+++ homework/13/13_1.py
@@ -7,7 +7,7 @@ class Human:
         self.last_name = last_name
 
     def __str__(self):
-        return f"{self.gender} {self.age} {self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name}"
 
 
 class Student(Human):
@@ -28,7 +28,10 @@ class Group:
         self.group = set()
 
     def add_student(self, student):
-        self.group.add(student)
+        if len(self.group) < 10:
+            self.group.add(student)
+        else:
+            raise MaxNumberOfStudent
 
     def delete_student(self, last_name):
         student_for_delete = self.find_student(last_name)
@@ -48,13 +51,40 @@ class Group:
         return f'Number:{self.number}\n{all_students} '
 
 
+class MaxNumberOfStudent(Exception):
+    pass
+
+
 st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
-st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+st2 = Student('Male', 25, 'John', 'Swift', 'AN145')
+st3 = Student('Female', 23, 'Ann', 'Berg', 'AN145')
+st4 = Student('Female', 22, 'Megan', 'Rich', 'AN145')
+st5 = Student('Male', 25, 'Mark', 'Zuckerberg', 'AN145')
+st6 = Student('Male', 26, 'Jake', 'Brenton', 'AN145')
+st7 = Student('Female', 25, 'Viktoria', 'Manson', 'AN145')
+st8 = Student('Male', 22, 'Michal', 'Billing', 'AN145')
+st9 = Student('Female', 25, 'Rachel', 'Bricks', 'AN145')
+st10 = Student('Female', 27, 'Monica', 'Bellucci', 'AN145')
+st11 = Student('Male', 27, 'Jane', 'Branson', 'AN145')
 print(st1)
 
 gr = Group('PD1')
 gr.add_student(st1)
 gr.add_student(st2)
+gr.add_student(st3)
+gr.add_student(st4)
+gr.add_student(st5)
+gr.add_student(st6)
+gr.add_student(st7)
+gr.add_student(st8)
+gr.add_student(st9)
+gr.add_student(st10)
+
+try:
+    gr.add_student(st11)
+except MaxNumberOfStudent:
+    print("Maximum number of students in a group is 10!")
+
 print(gr)
 gr.find_student('Jobs')
 assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
@@ -65,3 +95,4 @@ gr.delete_student('Taylor')
 print(gr)  # Only one student
 #
 gr.delete_student('Taylor')  # No error!
+
