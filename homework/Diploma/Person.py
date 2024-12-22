9bbd33b7a5f521a08729752cdfcd7e6818f6f927
Bohdan Danylko <malyyboh@gmail.com>
2024-12-22 20:11:07 +0100

    Diploma (full version)

--- homework/Diploma/Person.py
+++ homework/Diploma/Person.py
@@ -1,5 +1,4 @@
 from datetime import date, datetime
-from colorama import Fore, Back, Style
 
 
 class DataPersons:
@@ -7,28 +6,34 @@ class DataPersons:
         self.data_persons = set()
 
     def __str__(self):
-        all_persons = ''
+        all_persons = ""
         for person in self.data_persons:
-            all_persons += f"{str(person)}\n"
+            all_persons += (f"{person.name} {person.last_name if person.last_name != '-' else ''} {person.father_name if person.father_name != '-' else ''} "
+                            f"{person.calc_age()}, {'чоловік.' if person.gender == 'm' else 'жінка.'} {'Народився' if person.gender == 'm' else 'Народилася'} {person.birthday}. "
+                            f"{'Помер' if person.gender == 'm' and person.date_of_death != '-' else 'Померла' if person.gender == 'f' and person.date_of_death != '-' else ''} {person.date_of_death + '.' if person.date_of_death != '-' else ''}\n")
         return all_persons
 
     def add_person(self, person):
         self.data_persons.add(person)
 
     def find_person(self, *args):
-        find_person_str = ''
+        find_person_str_lst = []
         for person in self.data_persons:
             for i in args:
                 if i in person.name or i in person.last_name or i in person.father_name:
-                    find_person_str += f'{person.name} {person.last_name} {person.father_name} {person.calc_age()} {"чоловік." if person.gender == 'm' else "жінка."} {"Народився" if person.gender == 'm' else "Народилася"} {person.birthday}\n'
-        if find_person_str:
-            return find_person_str
-        else:
-            return f'{Fore.LIGHTRED_EX}Записи з такими даними відсутні!{Style.RESET_ALL}'
+                    find_person_str = (f"{person.name} {person.last_name if person.last_name != '-' else ''} {person.father_name if person.father_name != '-' else ''} "
+                                       f"{person.calc_age()}, {'чоловік.' if person.gender == 'm' else 'жінка.'} {'Народився' if person.gender == 'm' else 'Народилася'} {person.birthday}. "
+                                       f"{'Помер' if person.gender == 'm' and person.date_of_death != '-' else 'Померла' if person.gender == 'f' and person.date_of_death != '-' else ''} {person.date_of_death + '.' if person.date_of_death != '-' else ''}\n")
+                    if find_person_str not in find_person_str_lst:
+                        find_person_str_lst.append(find_person_str)
+        find_person = ''
+        for person_item in find_person_str_lst:
+            find_person += person_item
+        return find_person
 
 
 class Person:
-    def __init__(self, name, birthday, gender, last_name='', father_name='', date_of_death=''):
+    def __init__(self, name, birthday, gender, last_name='-', father_name='-', date_of_death='-'):
         self.name = name
         self.birthday = birthday
         self.gender = gender
@@ -37,31 +42,31 @@ class Person:
         self.date_of_death = date_of_death
 
     def __str__(self):
-        return f"{self.name} {self.birthday} {self.gender} {self.last_name} {self.father_name} {self.date_of_death} "
+        return f"{self.name} {self.birthday} {self.gender} {self.last_name} {self.father_name} {self.date_of_death}"
 
     def correct_date(self):
         self.birthday = self.birthday.strip()
-        self.date_of_death = self.date_of_death.strip()
         for i in self.birthday:
-            if i not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                self.birthday = self.birthday.replace(i, '.')
-        for i in self.date_of_death:
-            if i not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-                self.date_of_death = self.date_of_death.replace(i, '.')
+            if i not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                self.birthday = self.birthday.replace(i, ".")
+        if self.date_of_death != "-":
+            self.date_of_death = self.date_of_death.strip()
+            for i in self.date_of_death:
+                if i not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                    self.date_of_death = self.date_of_death.replace(i, ".")
 
     def calc_age(self):
-        # today = date.today().strftime('%d.%m.%Y')
         birthday_date_lst = self.birthday.split('.')
         birthday = datetime(int(birthday_date_lst[2]), int(birthday_date_lst[1]), int(birthday_date_lst[0]))
 
-        if not self.date_of_death:
+        if self.date_of_death == "-":
             today = date.today()
             age = today.year - birthday.year - ((today.month, today.day) < (birthday.month, birthday.day))
         else:
             death_date_lst = self.date_of_death.split('.')
             death_date = datetime(int(death_date_lst[2]), int(death_date_lst[1]), int(death_date_lst[0]))
             age = death_date.year - birthday.year - (
-                        (death_date.month, death_date.day) < (birthday.month, birthday.day))
+                    (death_date.month, death_date.day) < (birthday.month, birthday.day))
 
         if age % 10 == 1:
             if age == 11:
@@ -77,5 +82,3 @@ class Person:
             age = f"{age} років"
 
         return age
-
-
--- homework/Diploma/main.py
+++ homework/Diploma/main.py
@@ -3,17 +3,19 @@ from colorama import Fore, Back, Style
 import codecs
 
 
-print(f"{Fore.LIGHTCYAN_EX}РОБОТА З БАЗОЮ ДАНИХ ЛЮДЕЙ{Style.RESET_ALL}")
+print(f"{Fore.BLACK}{Back.LIGHTWHITE_EX}Програма для роботи з даними про людей{Style.RESET_ALL}")
 
 all_records = DataPersons()
 
 while True:
-    print(f"{Fore.LIGHTYELLOW_EX}Введіть відповідну команду для роботи з базою даних:{Style.RESET_ALL}")
+    print(f"{Fore.LIGHTCYAN_EX}{Back.BLACK}Введіть відповідну команду для роботи з даними:{Style.RESET_ALL}")
     print(f"1) для додавання нового запису введіть:{Fore.LIGHTRED_EX} add{Style.RESET_ALL}")
-    print(f"2) для збереження введених даних данних введіть:{Fore.LIGHTRED_EX} save{Style.RESET_ALL}")
-    print(f"3) для пошуку запису по імені, прізвищу чи по-батькові введіть:{Fore.LIGHTRED_EX} search{Style.RESET_ALL}")
-    print(f"4) для переляду всіх записів з бази даних введіть:{Fore.LIGHTRED_EX} show all{Style.RESET_ALL}")
-    print(f"5) для виходу з програми введіть:{Fore.LIGHTRED_EX} exit{Style.RESET_ALL}")
+    print(f"2) для пошуку в доданих записах по імені, прізвищу чи по-батькові введіть:{Fore.LIGHTRED_EX} search{Style.RESET_ALL}")
+    print(f"3) для збереження введених даних до файлу данних введіть:{Fore.LIGHTRED_EX} save{Style.RESET_ALL}")
+    print(f"4) для пошуку записів в файлі по імені, прізвищу чи по-батькові введіть:{Fore.LIGHTRED_EX} search in file{Style.RESET_ALL}")
+    print(f"5) для переляду всіх доданих записів введіть:{Fore.LIGHTRED_EX} show all{Style.RESET_ALL}")
+    print(f"6) для переляду всіх записів з файлу введіть:{Fore.LIGHTRED_EX} show all in file{Style.RESET_ALL}")
+    print(f"7) для виходу з програми введіть:{Fore.LIGHTRED_EX} exit{Style.RESET_ALL}")
     user_input = input(f"{Back.LIGHTCYAN_EX}{Fore.BLACK}Введіть команду:{Style.RESET_ALL} ")
 
     def add_person():
@@ -23,13 +25,28 @@ while True:
         last_name = input("Введіть прізвище (щоб пропустити натисніть Enter): ").title()
         father_name = input("Введіть по-батькові (щоб пропустити натисніть Enter): ").title()
         date_of_death = input("Введіть дату смерті (щоб пропустити натисніть Enter): ")
-        obj = Person(name=name, birthday=birthday, gender='m' if gender == "чоловік" else "f", last_name=last_name, father_name=father_name, date_of_death=date_of_death)
+        obj = Person(name=name, birthday=birthday, gender="m" if gender == "чоловік" else "f", last_name="-" if last_name=='' else last_name, father_name='-' if father_name=='' else father_name, date_of_death='-' if  date_of_death=='' else  date_of_death)
         obj.correct_date()
         all_records.add_person(obj)
 
+    def search_added_person(records: DataPersons):
+        if records.data_persons:
+            user_search = input(f"{Back.LIGHTCYAN_EX}{Fore.BLACK}Введіть імя, прізвище чи по-батькові:{Style.RESET_ALL} ").title()
+            user_search_lst = user_search.split()
+            find_records_print = all_records.find_person(*user_search_lst)
+            if not find_records_print:
+                user_search = input(f"{Fore.LIGHTRED_EX}Дані відустні! Продовжити пошук? (Yes or No):{Style.RESET_ALL} ")
+                if user_search.lower() == "yes":
+                    return search_added_person(all_records)
+            else:
+                return print(f"{Fore.LIGHTGREEN_EX}{find_records_print}")
+        else:
+            return print(f"{Fore.LIGHTRED_EX}Жодних доданих записів для пошуку не має! Спочатку додайте дані!{Style.RESET_ALL}")
+
     def save_person(records: DataPersons):
         if records.data_persons:
-            with codecs.open('db.txt', 'a', 'utf-8') as db:
+            input_file_name = input(f"{Back.LIGHTCYAN_EX}{Fore.BLACK}Задайте назву текстового файлу для збереження:{Style.RESET_ALL} ")
+            with codecs.open(f"db/{input_file_name}.txt", "a", "utf-8") as db:
                 for item in records.data_persons:
                     db.write(f"{str(item)}\n")
             save_message = f"{Fore.LIGHTGREEN_EX}Дані збережено!{Style.RESET_ALL}"
@@ -38,44 +55,88 @@ while True:
         return print(save_message)
 
     def search_person():
-        with codecs.open('db.txt', 'r', 'utf-8') as db:
-            db_lines = db.readlines()
-        find_records = DataPersons()
-        user_search = input("Введіть імя, прізвище чи по-батькові: ").title()
-        for line in db_lines:
-            args = line.split()
-            find_obj = Person(*args)
-            find_records.add_person(find_obj)
-        return print(find_records.find_person(user_search))
+        input_file_name = input(f"{Back.LIGHTCYAN_EX}{Fore.BLACK}Введіть назву файлу для пошуку:{Style.RESET_ALL} ")
+        try:
+            with codecs.open(f"db/{input_file_name}.txt", "r", "utf-8") as db:
+                db_lines = db.readlines()
+        except FileNotFoundError:
+            print(f"{Fore.LIGHTRED_EX}Файла не знайдено!{Style.RESET_ALL}")
+
+        else:
+            find_records = DataPersons()
+            user_search = input(f"{Back.LIGHTCYAN_EX}{Fore.BLACK}Введіть імя, прізвище чи по-батькові:{Style.RESET_ALL} ").title()
+            user_search_lst = user_search.split()
+            find_records_print = ""
+            for line in db_lines:
+                args = line.split()
+                find_obj = Person(*args)
+                find_records.add_person(find_obj)
+                find_records_print = find_records.find_person(*user_search_lst)
+            if not find_records_print:
+                user_search = input(f"{Fore.LIGHTRED_EX}Дані відустні! Продовжити пошук? (Yes or No):{Style.RESET_ALL} ")
+                if user_search.lower() == "yes":
+                    return search_person()
+            else:
+                return print(f"{Fore.LIGHTGREEN_EX}{find_records_print}")
+
+    def show_all_added_persons(records: DataPersons):
+        if records.data_persons:
+            return print(f"{Fore.LIGHTGREEN_EX}{records.__str__()}")
+        else:
+            return print(f"{Fore.LIGHTRED_EX}Дані для перегляду відсутні! Спочатку додайте дані!{Style.RESET_ALL}")
+
+    def show_all_persons():
+        input_file_name = input(f"{Back.LIGHTCYAN_EX}{Fore.BLACK}Введіть назву файлу для відображення всіх записів:{Style.RESET_ALL} ")
+        try:
+            with codecs.open(f"db/{input_file_name}.txt", "r", "utf-8") as db:
+                db_lines = db.readlines()
+        except FileNotFoundError:
+            print(f"{Fore.LIGHTRED_EX}Файла не знайдено!{Style.RESET_ALL}")
+        else:
+            all_records_in_file = DataPersons()
+            for line in db_lines:
+                args = line.split()
+                record_obj = Person(*args)
+                all_records_in_file.add_person(record_obj)
+
+            if not all_records_in_file.data_persons:
+                print(f"{Fore.LIGHTRED_EX}В даному файлі не має жодного запису!{Style.RESET_ALL} ")
+            else:
+                return print(f"{Fore.LIGHTGREEN_EX}{all_records_in_file.__str__()}")
 
     def exit_prog():
         if all_records.data_persons:
-            save_input = input("Введена дані не збережені! Введіть 'save' для збереження або 'not save', щоб вийти без збереження: ")
+            save_input = input(f"{Fore.LIGHTRED_EX}Введені дані не збережені! Введіть 'save' для збереження або 'not save', щоб вийти без збереження:{Style.RESET_ALL} ")
             if save_input.lower() == "save":
                 save_person(all_records)
         exit()
 
+
     if user_input.lower() == "add":
         add_person()
+
+    if user_input.lower() == "search":
+        search_added_person(all_records)
 
     if user_input.lower() == "save":
         save_person(all_records)
         all_records = DataPersons()
 
-    if user_input.lower() == "search":
+    if user_input.lower() == "search in file":
         search_person()
+
+    if user_input.lower() == "show all":
+        show_all_added_persons(all_records)
+
+    if user_input.lower() == "show all in file":
+        show_all_persons()
 
     if user_input.lower() == "exit":
         exit_prog()
 
-    # if user_input == "show all":
-    #     with codecs.open('db.txt', 'r', 'utf-8') as db:
-    #         db_lines = db.readlines()
-    #         args = []
-    #         for line in db_lines:
-    #             args = line.split()
-    #             obj = Person(*args)
-    #             print(obj.__str__())
+    if user_input.lower() not in ["add", "search", "save", "search in file", "show all", "show all in file", "exit"]:
+        print(f"{Fore.LIGHTRED_EX}Неправильна назва команди!{Style.RESET_ALL}")
+
 
 
 
